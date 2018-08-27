81ca30f0cbcc2e2fe2e1d2b85a51f0cb3770661b
anh_nqv <anh_nqv@hicas.com.vn>
2018-08-27 19:06:21 +0700

    push code

---
+++ Sample Projects/InitTool/AcadCmds.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Windows;
+
+[assembly: CommandClass(typeof(InitTool.AcadFuncs))]
+namespace InitTool
+{
+    public class AcadFuncs
+    {
+		[CommandMethod("MyCommand")]
+		public void MyCommandFunc()
+		{
+			MessageBox.Show("My Comamnd is called!");
+		}
+    }
+}
