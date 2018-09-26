3fb340c138c53e2b745b2eff410a41e6403a752f
anh_nqv <anh_nqv@hicas.com.vn>
2018-09-26 08:29:05 +0700

    push document

---
+++ OOP_and_C#/Tut03/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tut03
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Shape s1 = new Rectangle(10, 12);
+			Shape s2 = new Square(10);
+
+			Console.WriteLine(s1.Area());
+			Console.WriteLine(s2.Area());
+		}
+	}
+
+	interface Shape
+	{
+		double Area();
+	}
+
+	class Rectangle : Shape
+	{
+		protected double width;
+		protected double height;
+
+		public Rectangle(double _w, double _h)
+		{
+			width = _w;
+			height = _h;
+		}
+
+		public double Area()
+		{
+			return width * height;
+		}
+	}
+
+	class Square : Rectangle, Shape
+	{
+		public Square(double _w) : base(_w, _w)
+		{
+		}
+
+		public new double Area()
+		{
+			Console.WriteLine("Calculate Area of Square:");
+			return base.Area();
+		}
+	}
+}
---
+++ OOP_and_C#/Tut04/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tut04
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			UseTuple();
+		}
+
+		static void UseList()
+		{
+			List<string> strs = new List<string>();
+			strs.Add("abc");
+			strs.Add("def");
+			strs.Add("xyz");
+
+			strs.RemoveAt(1);
+			strs.Insert(1, "zyx");
+
+			foreach(var str in strs)
+			{
+				Console.WriteLine(str);
+			}
+		}
+
+		static void UseQueue()
+		{
+			Queue<string> strs = new Queue<string>();
+			strs.Enqueue("abc");
+			strs.Enqueue("def");
+			strs.Enqueue("xyz");
+
+			foreach(var str in strs)
+			{
+				Console.WriteLine(str);
+			}
+
+			strs.Dequeue();
+		}
+
+		static void UseStack()
+		{
+			Stack<string> strs = new Stack<string>();
+			strs.Push("abc");
+			strs.Push("xyz");
+			strs.Push("dfx");
+
+			foreach(var str in strs)
+			{
+				Console.WriteLine(str);
+			}
+
+			strs.Pop();
+		}
+
+		static void UseTuple()
+		{
+			Tuple<string, int> pairs = new Tuple<string, int>("abc", 12);
+			Tuple<string, int> pairs2 = new Tuple<string, int>("abc", 10);
+			bool cmp = pairs.Equals(pairs2);
+
+			List<Tuple<string, int>> items = new List<Tuple<string, int>>();
+			items.Add(new Tuple<string, int>("abc", 12));
+			items.Add(new Tuple<string, int>("abc", 10));
+			items.Add(new Tuple<string, int>("dxy", 120));
+
+			var search_result = items.FindAll(x => "abc" == x.Item1);
+			var search_result2 = items.FindAll(x => 12 == x.Item2);
+		}
+
+		static void UseDictionary()
+		{
+			Dictionary<string, int> dic = new Dictionary<string, int>();
+			dic.Add("abc", 12);
+			dic.Add("dxy", 14);
+
+			Console.Write(dic["abc"]);
+		}
+	}
+}
